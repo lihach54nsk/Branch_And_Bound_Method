22ef13447a38937382ef909ec01fd6fe60abbe07
Luzyanin Alexey <261-96@mail.ru>
2021-05-03 19:43:42 +0700

    Добавьте файлы проекта.

---
+++ Branch_And_Bound_Method/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace Branch_And_Bound_Method
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewInput = new System.Windows.Forms.DataGridView();
+            this.P = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.S = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.t = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.G = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewOutput = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOutput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewInput
+            // 
+            this.dataGridViewInput.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewInput.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.P,
+            this.S,
+            this.t,
+            this.G});
+            this.dataGridViewInput.Location = new System.Drawing.Point(13, 13);
+            this.dataGridViewInput.Name = "dataGridViewInput";
+            this.dataGridViewInput.RowTemplate.Height = 25;
+            this.dataGridViewInput.Size = new System.Drawing.Size(449, 206);
+            this.dataGridViewInput.TabIndex = 0;
+            // 
+            // P
+            // 
+            this.P.HeaderText = "P";
+            this.P.Name = "P";
+            // 
+            // S
+            // 
+            this.S.HeaderText = "S";
+            this.S.Name = "S";
+            // 
+            // t
+            // 
+            this.t.HeaderText = "t";
+            this.t.Name = "t";
+            // 
+            // G
+            // 
+            this.G.HeaderText = "G";
+            this.G.Name = "G";
+            // 
+            // dataGridViewOutput
+            // 
+            this.dataGridViewOutput.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewOutput.Location = new System.Drawing.Point(509, 13);
+            this.dataGridViewOutput.Name = "dataGridViewOutput";
+            this.dataGridViewOutput.RowTemplate.Height = 25;
+            this.dataGridViewOutput.Size = new System.Drawing.Size(406, 206);
+            this.dataGridViewOutput.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(927, 274);
+            this.Controls.Add(this.dataGridViewOutput);
+            this.Controls.Add(this.dataGridViewInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOutput)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewInput;
+        private System.Windows.Forms.DataGridViewTextBoxColumn P;
+        private System.Windows.Forms.DataGridViewTextBoxColumn S;
+        private System.Windows.Forms.DataGridViewTextBoxColumn t;
+        private System.Windows.Forms.DataGridViewTextBoxColumn G;
+        private System.Windows.Forms.DataGridView dataGridViewOutput;
+    }
+}
+
